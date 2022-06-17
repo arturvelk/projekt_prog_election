68a3b0f3e77ecbc3fd15ee5401d520b66644c970
arturvelk <64686836+arturvelk@users.noreply.github.com>
2022-06-17 20:34:13 +0200

    akkor egyben

--- app.py
+++ app.py
@@ -8,6 +8,54 @@ from streamlit_folium import folium_static
 from fun import state_style, style_function
 
 
+def state_style(data, state,function=False):
+    """
+    Returns the style for a state in a given year
+    """
+    
+    state_result = data[state]
+    
+    #Set state colour
+    if max(state_result, key=state_result.get) == "SZDSZ":
+        color =  "#0783c7"#blue
+    elif max(state_result, key=state_result.get) == "FKGP":
+        color = "#445f2b"
+    elif max(state_result, key=state_result.get) == "MDF":
+        color = "#3c8a5a"
+    elif max(state_result, key=state_result.get) == "MSZP":
+        color = '#e71a29' #red
+    elif max(state_result, key=state_result.get) == "FIDESZ":
+        color = "#fd8100"
+        
+    
+    #Set state style
+    if function == False:
+        # Format for style_dictionary
+        state_style = {
+            'opacity': 1,
+            'color': color,
+        } 
+    else:
+        # Format for style_function
+        state_style = {
+             'fillOpacity': 1,
+             'weight': 1,
+             'fillColor': color,
+             'color': '#000000'}    
+  
+    return state_style
+
+def style_function(feature):
+    """
+    style_function used by the GeoJson folium function
+    """
+
+    state = feature['properties']['NAME_1']
+    style = state_style(state,function=True)
+    
+    return style
+
+
 st.write("My First Streamlit Web App, csicskavok")
 
 data = json.load(open("sample.json", encoding = "UTF-8"))
