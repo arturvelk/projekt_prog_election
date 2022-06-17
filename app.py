d2a263faebfe3a455a377b100286b4d96eaf7b94
arturvelk <64686836+arturvelk@users.noreply.github.com>
2022-06-17 19:45:02 +0200

    basic map

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ st.write("My First Streamlit Web App, csicskavok")
 hu_shape = gpd.read_file('map/HUN_adm1.shp')[["NAME_1","geometry"]]
 
 m = folium.Map(location=[47, 20],zoom_start=7) 
-choropleth =folium.GeoJson(data= hu_shape.to_json())
-folium.TileLayer('cartodbdark_matter',name="dark mode",control=True).add_to(m)
-m.add_child(choropleth)
+#choropleth =folium.GeoJson(data= hu_shape.to_json())
+#folium.TileLayer('cartodbdark_matter',name="dark mode",control=True).add_to(m)
+#m.add_child(choropleth)
 st.title("kezdetleges map")
 folium_static(m)
