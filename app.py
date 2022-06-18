733648e4034686d766049d49998786a0ee51182c
saraszunyog <63114620+saraszunyog@users.noreply.github.com>
2022-06-18 13:54:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def highlight_style(feature):
 
 
 
-st.write("My First Streamlit Web App, csicskavok")
+st.write("My First Streamlit Web App, csicskavok, csicska")
 
 #hu_shape = json.load(open('hu_distrcit.geojson', encoding = "UTF-8"))
 
